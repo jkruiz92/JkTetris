5f0a6a9e53557a7cdb5f4eb8b73f1ad3a9bc0864
Juan Carlos <96532401+jkruiz92@users.noreply.github.com>
2022-11-29 15:04:37 +0100

    Update tetris.py
    Text option added into menu

--- main/tetris.py
+++ main/tetris.py
@@ -8,9 +8,16 @@ class tetris():
         print(os.getcwd())
 
         self.menu = turtle.Screen()
-        self.menu.setup(500, 500)
+        self.menu.setup(500, 400)
         self.menu.title("JkTetrisGame     @jkruiz")
         self.menu.bgcolor("black")
+        turtle.hideturtle()
+        turtle.penup()
+        turtle.setposition(-150, 100)
+        turtle.color("green")
+        turtle.write("Start Play!", move=False, font=("Verdana", 20, "normal"),  align="left")
+        turtle.setposition(-150, 50)
+        turtle.write("Close", move=False, font=("Verdana", 20, "normal"),  align="left")
         self.menu.listen()
         self.menu.onkey(self.playing, "Up")
         self.menu.mainloop()
@@ -20,7 +27,7 @@ class tetris():
         print("Playing...")
         play_screen = turtle.Screen()
         play_screen.setup(350, 500)
-        play_screen.bgcolor("green")
+        play_screen.bgcolor("light green")
 
 
 if __name__ == "__main__":
